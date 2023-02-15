b37e800788c4ed43140a782451c57e39eea8ad34
LichKing1305 <m.salnikovs@gmail.com>
2023-02-15 15:54:11 +0100

    Create Powerup.cs

---
+++ project lift off/GXPEngine/Powerup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GXPEngine;
+public class Powerup : AnimationSprite
+{
+
+    public Powerup() : base("circle.png", 1, 1)
+    {
+        spawn();
+    }
+
+    void Update()
+    {
+
+
+
+    }
+
+    void spawn()
+    {
+        x = Utils.Random(0, game.width - this.width);
+        y = Utils.Random(500, game.height - this.height);
+    }
+    public void Pickup()
+    {
+        spawn();
+    }
+
+}
