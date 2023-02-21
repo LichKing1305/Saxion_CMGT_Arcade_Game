fa47be93e7200588230af2ed49cf6555d527e9f5
LichKing1305 <m.salnikovs@gmail.com>
2023-02-21 09:09:11 +0100

    Created CoinPickup class and started work on the timer for power up spawning

---
+++ project lift off/GXPEngine/CoinPIckup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using GXPEngine;
+
+public class CoinPickup : AnimationSprite 
+{ 
+    public CoinPickup(string filename, int cols, int rows) : base(filename, cols, rows) 
+    { 
+    
+    }
+
+    public CoinPickup() : base("coin.jpg", 1, 1)
+    {
+
+    }
+}
